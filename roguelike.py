e781bd5ff6aca6649344cfaaf7ad28a8efa0bb81
AdrianE92 <adrian_eriksen92@hotmail.com>
2018-10-22 19:28:29 +0200

    Added random dungeon generator

--- roguelike.py
+++ roguelike.py
@@ -1,4 +1,5 @@
 import tdl
+from random import randint
 
 #Set window size and fps
 SCREEN_WIDTH = 80
@@ -8,6 +9,10 @@ REALTIME = False
 #Set map size
 MAP_WIDTH = 80
 MAP_HEIGHT = 45
+#Set room size
+ROOM_MAX_SIZE = 10
+ROOM_MIN_SIZE = 6
+MAX_ROOMS = 30
 
 #Set colors
 color_dark_wall = (0, 0, 100)
@@ -46,19 +51,25 @@ class Tile:
             block_sight = blocked
         
         self.block_sight = block_sight
-        
-def make_map():
-    global my_map
-    #fill map with unblocked tiles 
-    my_map = [[ Tile(False)
-        for y in range(MAP_HEIGHT)]
-            for x in range(MAP_WIDTH)]
-            
-    my_map[30][22].blocked = True
-    my_map[30][22].block_sight = True
-    my_map[50][22].blocked = True
-    my_map[50][22].block_sight = True
 
+class Rect:
+    #Initialize map variables
+    def __init__(self, x, y, w, h):
+        self.x1 = x
+        self.y1 = y
+        self.x2 = x + w
+        self.y2 = y + h
+    
+    def center(self):
+        center_x = (self.x1 + self.x2)//2
+        center_y = (self.y2 + self.y2)//2
+        return (center_x, center_y)
+    
+    def intersect(self, other):
+        #Returns True if this rectangle intersects with another one
+        return (self.x1 <= other.x2 and self.x2 >= other.x1 and 
+                self.y1 <= other.y2 and self.y2 >= other.y1)
+            
 def handle_keys(realtime):
     global playerx, playery
     
@@ -72,6 +83,8 @@ def handle_keys(realtime):
                 return
     
     else: 
+        #Turn based
+        user_input = tdl.event.key_wait()
         if user_input.key == 'ENTER' and user_input.alt:
             #Alt+Enter: toggle fullscreen
             tdl.set_fullscreen(not tdl.get_fullscreen())
@@ -101,7 +114,91 @@ def render_all():
     
     #Blit the contents of 'con' to the root console and present it
     root.blit(con, 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, 0, 0)
+
+def make_map():
+    global my_map
     
+    #fill map with unblocked tiles 
+    my_map = [[ Tile(True) 
+        for y in range(MAP_HEIGHT)]
+            for x in range(MAP_WIDTH)]
+    
+    rooms = []
+    num_rooms = 0
+    
+    for r in range(MAX_ROOMS):
+        #Random width and height 
+        w = randint(ROOM_MIN_SIZE, ROOM_MAX_SIZE)
+        h = randint(ROOM_MIN_SIZE, ROOM_MAX_SIZE)
+        #Random position without going out of the boundaries of the map
+        x = randint(0, MAP_WIDTH-w-1)
+        y = randint(0, MAP_HEIGHT-h-1)
+        
+        #Rect() makes rectangles easier to work with
+        new_room = Rect(x, y, w, h)
+        
+        #Run through the other rooms and see if they intersect with this one
+        failed = False
+        for other_room in rooms:
+            if new_room.intersect(other_room):
+                failed = True
+                break
+        
+        if not failed:
+            #This means there are no intersections, so this room is valid
+            
+            #"paint" it to the map's tiles
+            create_room(new_room)
+            #Center coordinates of new room
+            (new_x, new_y) = new_room.center()
+            if num_rooms == 0:
+                #This is the first room, where the player starts
+                player.x = new_x
+                player.y = new_y
+            
+            else:
+                #Not the first room
+                #Connect to the previous room with a tunnel
+                
+                #Center coordinates of previous rooms
+                (prev_x, prev_y) = rooms[num_rooms-1].center()
+                
+                #Flip a coin
+                if randint(0, 1) == 1:
+                    #First move vertically, then horizontally
+                    create_h_tunnel(prev_x, new_x, prev_y)
+                    create_v_tunnel(prev_y, new_y, new_x)
+                else:
+                    #First move horizontally, then vertically
+                    create_v_tunnel(prev_y, new_y, prev_x)
+                    create_h_tunnel(prev_x, new_x, new_y)
+            
+            #Append the new room to list
+            rooms.append(new_room)
+            num_rooms += 1
+
+def create_room(room):
+    global my_map
+    
+    for x in range(room.x1+1, room.x2):
+        for y in range(room.y1+1, room.y2):
+            my_map[x][y].blocked = False
+            my_map[x][y].block_sight = False
+
+def create_h_tunnel(x1, x2, y):
+    global my_map
+    #Create horizontal tunnel 
+    for x in range(min(x1, x2), max(x1, x2)+1):
+        my_map[x][y].blocked = False
+        my_map[x][y].block_sight = False
+
+def create_v_tunnel(y1, y2, x):
+    global my_map
+    #Create vertical tunnel
+    for y in range(min(y1, y2), max(y1, y2)+1):
+        my_map[x][y].blocked = False
+        my_map[x][y].block_sight = False
+
 
 #####################################
 #Initialization & Main Loop         #
